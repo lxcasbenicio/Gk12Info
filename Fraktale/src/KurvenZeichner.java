33dd8d17696cbd8eeea1168d7a93761916093bba
lucas.karig <lucas.karig@PC020367.gymtsdom.local>
2020-11-25 11:32:26 +0100

    hlo

--- Fraktale/src/KurvenZeichner.java
+++ Fraktale/src/KurvenZeichner.java
@@ -1,69 +1,116 @@
 
 public class KurvenZeichner {
-	
+
 	private Schildkroete schildkroete;
-	
-	public KurvenZeichner(Schildkroete schildkroete){
+
+	public KurvenZeichner(Schildkroete schildkroete) {
 		this.schildkroete = schildkroete;
-		schildkroete.positionieren(400, 200);
+		schildkroete.positionieren(200, 400);
 	}
 
-	public void zackenKoch(int n,double s) {
-		zackenKochInitiator(n,s);
+	public void zackenKoch(int n, double s) {
+		zackenKochInitiator(n, s);
 	}
-	
-	/** Zeichnet die Startfigur mit einem Aufruf des Generators
-	  * fuer jede Kante.
-	  */
-	private void zackenKochInitiator(int n, double s){
+
+	/**
+	 * Zeichnet die Startfigur mit einem Aufruf des Generators fuer jede Kante.
+	 */
+	private void zackenKochInitiator(int n, double s) {
 		zackenKochGenerator(n, s);
 	}
-	
-	/** Zeichnet eine Kante der gegebenen Rekursionsebene.
-	  * @param s aktuelle Kantenlaenge.
-	  * @param n aktuelle Rekursionstiefe.
-	  */
-	private void zackenKochGenerator(int n,double s){
-	}
-	
-	public void quadratKoch(int n,double s) {
-		quadratKochInitiator(n, s);
-	}
-	
-	/** Zeichnet die Startfigur mit einem Aufruf des Generators
-	  * fuer jede Kante.
-	  */
-	private void quadratKochInitiator(int n, double s) {
-		
+
+	/**
+	 * Zeichnet eine Kante der gegebenen Rekursionsebene.
+	 * 
+	 * @param s aktuelle Kantenlaenge.
+	 * @param n aktuelle Rekursionstiefe.
+	 */
+	private void zackenKochGenerator(int n, double s) {
+		if (n == 0) {
+			schildkroete.laufen(s);
+		} else {
+			schildkroete.drehen(-90);
+			zackenKochGenerator(n - 1, s / 3);
+			schildkroete.drehen(+60);
+			zackenKochGenerator(n - 1, s / 3);
+			schildkroete.drehen(-120);
+			zackenKochGenerator(n - 1, s / 3);
+			schildkroete.drehen(+60);
+			zackenKochGenerator(n - 1, s / 3);
+
+		}
 	}
 
-	/** Zeichnet eine Kante der gegebenen Rekursionsebene.
-	  * @param s aktuelle Kantenlaenge.
-	  * @param n aktuelle Rekursionstiefe.
-	  */
+	public void quadratKoch(int n, double s) {
+		quadratKochInitiator(n, s);
+
+		if (n == 0) {
+			schildkroete.laufen(s);
+		} else {
+			schildkroete.drehen(-90);
+			zackenKochGenerator(n - 1, s / 3);
+			schildkroete.drehen(+90);
+			zackenKochGenerator(n - 1, s / 3);
+			schildkroete.drehen(-90);
+			zackenKochGenerator(n - 1, s / 3);
+			schildkroete.drehen(-90);
+			zackenKochGenerator(n - 1, s / 3);
+			schildkroete.drehen(-90);
+			zackenKochGenerator(n - 1, s / 3);
+		
+			
+		}
+	}
+
+	/**
+	 * Zeichnet die Startfigur mit einem Aufruf des Generators fuer jede Kante.
+	 */
+	private void quadratKochInitiator(int n, double s) {
+
+	}
+
+	/**
+	 * Zeichnet eine Kante der gegebenen Rekursionsebene.
+	 * 
+	 * @param s aktuelle Kantenlaenge.
+	 * @param n aktuelle Rekursionstiefe.
+	 */
 	private void quadratKochGenerator(int n, double s) {
 	}
-	
+
 	public void schneeflocke(int n, double s) {
-		schneeflockeInitiator(n,s);
+		 schneeflockeInitiator(n, s);
 	}
-	
-	/** Zeichnet die Startfigur mit einem Aufruf des Generators
-	  * fuer jede Kante.
-	  */
+	/**
+	 * Zeichnet die Startfigur mit einem Aufruf des Generators fuer jede Kante.
+	 */
 	private void schneeflockeInitiator(int n, double s) {
-		schneeflockeGenerator(n,s);
+		schneeflockeGenerator(n, s);
 		schildkroete.drehen(-120);
-		schneeflockeGenerator(n,s);
+		schneeflockeGenerator(n, s);
 		schildkroete.drehen(-120);
-		schneeflockeGenerator(n,s);	
+		schneeflockeGenerator(n, s);
 	}
 
-	/** Zeichnet eine Kante der gegebenen Rekursionsebene.
-	  * @param s aktuelle Kantenlaenge.
-	  * @param n aktuelle Rekursionstiefe.
-	  */
+	/**
+	 * Zeichnet eine Kante der gegebenen Rekursionsebene.
+	 * 
+	 * @param s aktuelle Kantenlaenge.
+	 * @param n aktuelle Rekursionstiefe.
+	 */
 	private void schneeflockeGenerator(int n, double s) {
+		if (n == 0) {
+			schildkroete.laufen(s);
+		} else {
+			schneeflockeGenerator(n - 1, s / 3);
+			schildkroete.drehen(60);
+			schneeflockeGenerator(n - 1, s / 3);
+			schildkroete.drehen(-120);
+			schneeflockeGenerator(n - 1, s / 3);
+			schildkroete.drehen(+60);
+			schneeflockeGenerator(n - 1, s / 3);
+			
+		}
 	}
 
 }
--- Fraktale/src/Main.java
+++ Fraktale/src/Main.java
@@ -1,20 +1,18 @@
 
 public class Main {
-	
-	
-	
-	public static void main(String args[]){
-		
+
+	public static void main(String args[]) {
+
 		SchildkroetenGrafik schildkroetenGrafik = new SchildkroetenGrafik("Kochkurve", 700, 700);
 		Schildkroete schildkroete = schildkroetenGrafik.createSchildkroete();
 
 		schildkroete.drehen(90);
 		KurvenZeichner zeichner = new KurvenZeichner(schildkroete);
 		zeichner.zackenKoch(1, 150);
-
+		zeichner.quadratKoch(1, 150);
+		zeichner.schneeflocke(3, 250);
 		schildkroetenGrafik.warten(); // wartet bis das Fenster geschlossen wird
-		
-		
+
 	}
 
 }
